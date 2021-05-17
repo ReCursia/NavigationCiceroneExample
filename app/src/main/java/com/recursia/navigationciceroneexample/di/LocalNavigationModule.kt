dbaf0c083474e84ea104ca5209c287b85e8ddbe9
Alexander Silinsky <Alexander.Silinsky@kaspersky.com>
2021-05-18 00:19:43 +0300

    implementation progress (set activity scope)

--- app/src/main/java/com/recursia/navigationciceroneexample/di/LocalNavigationModule.kt
+++ app/src/main/java/com/recursia/navigationciceroneexample/di/LocalNavigationModule.kt
@@ -4,14 +4,16 @@ import com.recursia.navigationciceroneexample.common.LocalCiceroneHolder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ActivityComponent::class)
 class LocalNavigationModule {
 
     private val localCiceroneHolder: LocalCiceroneHolder = LocalCiceroneHolder()
 
     @Provides
+    @ActivityScoped
     fun provideLocalCiceroneHolder(): LocalCiceroneHolder = localCiceroneHolder
 }
